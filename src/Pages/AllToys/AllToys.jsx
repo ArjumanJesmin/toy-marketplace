f1403042f42e372d944f6e9a0f237803fafdff4d
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-05-18 11:40:22 +0600

    firebase configration and environment updated

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,11 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Button, Col, Container, Row, Table } from 'react-bootstrap';
+import TableBody from './TableBody';
 
 const AllToys = () => {
     return (
-        <div>
-            <h2>All Toys Page</h2>
-        </div>
+       <Container className='my-4'>
+        <Row>
+            <Col>
+            <Table  striped bordered hover>
+        <thead>
+          <tr>
+            <th>Seller</th>
+            <th>Toy Name</th>
+            <th>Sub-category</th>
+          </tr>
+        </thead>
+        <TableBody/>
+       
+      </Table>
+            </Col>
+        </Row>
+       </Container>
     );
 };
 
---
+++ src/Pages/AllToys/TableBody.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td><Button>View Details</Button></td>
+            </tr>
+        </tbody>
+
+    );
+};
+
+export default TableBody;
