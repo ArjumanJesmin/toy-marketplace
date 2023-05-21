2f8c1d3bb8c4ed36e8af09aef87ce14855e14a9a
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-05-21 17:30:43 +0600

    wow I have set the  search text

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,49 +1,77 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
-import {  Col, Container, Row, Table } from 'react-bootstrap';
+import { Button, Col, Container, Form, InputGroup, Row, Table } from 'react-bootstrap';
 import TableBody from './TableBody';
 import useTitle from '../../hooks/useTitle';
 
 
 
 const AllToys = () => {
+
   const [allToys, setAllToys] = useState([]);
+  const [search, setSearch] = useState('')
   useTitle('AllToys')
+
   useEffect(() => {
     fetch('https://baby-doll-server-side.vercel.app/toys')
       .then(res => res.json())
       .then(data => {
         setAllToys(data)
-        console.log(data)
+        // console.log(data)
       })
 
   }, [])
 
+
+  // search
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/searchToys/${search}`)
+    .then(res => res.json())
+    .then(data => {
+      setAllToys(data)
+      console.log(data)
+    })
+  }
+
   return (
-   
-      <Container className='my-4'>
-        <Row>
-          <Col>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Seller</th>
-                  <th>Name</th>
-                  <th>Toy Name</th>
-                  <th>available_quantity</th>
-                  <th>rating</th>
-                  <th>email</th>
-                  <th>image</th>
-                </tr>
-              </thead>
-              {
-                allToys.map((singleToy, index) => <TableBody index={index} singleToy={singleToy} key={singleToy._id} />)
-              }
-            </Table>
-          </Col>
-        </Row>
-      </Container>
+
+    <Container className='my-4'>
+      <div>
+        <InputGroup onChange={(e) => setSearch(e.target.value)} type='text' className="mb-3 w-50 mx-auto">
+          <Button onClick={handleSearch} variant="outline-secondary" id="button-addon1">
+            Button
+          </Button>
+          <Form.Control
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+
+      </div>
+
+      <Row>
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Seller</th>
+                <th>Name</th>
+                <th>Toy Name</th>
+                <th>available_quantity</th>
+                <th>rating</th>
+                <th>email</th>
+                <th>image</th>
+              </tr>
+            </thead>
+            {
+              allToys.map((singleToy, index) => <TableBody index={index} singleToy={singleToy} key={singleToy._id} />)
+            }
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
